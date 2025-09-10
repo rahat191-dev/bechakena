d73993edc9d08feb44ebf1e4f12999459debf41e
Rahat Hossain <rahat191.dev@gmail.com>
2025-09-10 10:13:26 +0600

    jb

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,6 +1,13 @@
-const hamburger = document.getElementById("hamburger");
+fetch("components/navbar.html")
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+   
+
+    const hamburger = document.getElementById("hamburger");
 const userPage = document.getElementById("user-page");
-hamburger.addEventListener('click'), () => {
+hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('open');
     userPage.classList.toggle('show');
-};
+});
+ });
--- assets/js/script.js
+++
@@ -1,5 +0,0 @@
-fetch("components/navbar.html")
-    .then(res => res.text())
-    .then(data => {
-        document.getElementById("navbar").innerHTML = data;
-    });
